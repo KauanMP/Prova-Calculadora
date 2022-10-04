97dcc545f0fdc77a6c320230613fd53c6e5ab252
Eduardo Gigliotti <99850450+GigliottiEduardo@users.noreply.github.com>
2022-10-03 22:28:32 -0300

    03.10 atualização no comando

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  operacao = '';
+  resultado = '';
 
   constructor() {}
+
+  ngOnInit(): void {}
+
+  adicionarValor(valor: string) {
+    this.operacao += valor;
+  }
+
+  limparOperacao(){
+    this.operacao = '';
+
+  }
+
+  limparMemoria(){
+    this.operacao = '';
+    this.resultado = '';
+
+  }
+
+  apagarCaracter(){
+    this.operacao = this.operacao.substring(0, this.operacao.length -1);
+
+  }
 
 }
