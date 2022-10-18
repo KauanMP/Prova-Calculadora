1fa171cc86004ba3b99f795c40b795246b98cea7
Eduardo Gigliotti <99850450+GigliottiEduardo@users.noreply.github.com>
2022-10-17 22:27:25 -0300

    17.10 calculadora tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Validators } from '@angular/forms';
+import { evaluate } from 'mathjs'
+
 
 @Component({
   selector: 'app-tab2',
@@ -9,14 +10,34 @@ import { Validators } from '@angular/forms';
 export class Tab2Page {
   operacao = '';
   resultado = '';
+  numero = false;
+  caracter = true;
+  caracteres = ['.', '/', '*', '+', '-'];
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  calcularOperacao() {
+    try {
+    this.resultado = evaluate(this.operacao);
+  } catch (err) {
+    this.resultado = 'inválido!;'
+  }  
+}
+
   adicionarValor(valor: string) {
-    this.operacao += valor;
+    this.caracter = this.caracteres.includes(valor);
+
+    if (!this.caracter) {
+      this.operacao += valor;
+      this.numero = true;
+    } else if (this.caracter && this.numero) {
+      this.operacao += valor;
+      this.numero = false;
+    }
   }
+
 
   limparOperacao(){
     this.operacao = '';
@@ -29,9 +50,20 @@ export class Tab2Page {
 
   }
 
-  apagarCaracter(){
+  apagarCaracter(){{
     this.operacao = this.operacao.substring(0, this.operacao.length -1);
 
   }
 
+  const ultimo = this.operacao.substring(this.operacao.length, 1);
+  this.caracter = this.caracteres.includes(ultimo);
+
+  console.log(ultimo);
+
+  if (!this.caracter){
+    this.numero = true;
+  } else {
+    this.numero = false;
+  }
+}
 }
